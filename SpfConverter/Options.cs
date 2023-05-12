b9ebe18e3de37cc83420c79de925013e627feec0
sichii <despereaux@live.com>
2023-05-12 13:34:32 -0400

    initial commit

---
+++ SpfConverter/Options.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+
+namespace SpfConverter;
+
+public class Options
+{
+    [Option('t', "tospf", Required = false, HelpText = "Converts the input to an SPF image")]
+    public bool ToSpf { get; set; }
+    
+    [Option('f', "fromspf", Required = false, HelpText = "Converts the input SPF image to a rgb565 image")]
+    public bool FromSpf { get; set; }
+
+    [Option(
+        'i',
+        "input",
+        Required = true,
+        HelpText = "The input path. To convert multiple input files to a multi-frame SPF, specify a directory containing multiple input files")]
+    public string Input { get; set; } = null!;
+
+    [Option(
+        'o',
+        "output",
+        Required = true,
+        HelpText = "The output path. To convert a multi-frame SPF to multiple images, specify a directory")]
+    public string Output { get; set; } = null!;
+}
---
+++ SpfConverter/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommandLine;
+using ImageMagick;
+using SpfConverter;
+using SpfConverter.Spf;
+
+Parser.Default.ParseArguments<Options>(args)
+      .WithParsed(
+          o =>
+          {
+              if (!o.FromSpf && !o.ToSpf)
+              {
+                  Console.WriteLine("Please specify either --tospf (-t), or --fromspf (-f)");
+
+                  return;
+              }
+              
+              var inputPaths = new List<string>();
+              
+              if(Directory.Exists(o.Input))
+                  inputPaths.AddRange(Directory.GetFiles(o.Input));
+              else if(File.Exists(o.Input))
+                  inputPaths.Add(o.Input);
+              else
+              {
+                  Console.WriteLine("Input path does not exist");
+
+                  return;
+              }
+
+              if (o.ToSpf)
+              {
+                  using var imageCollection = new MagickImageCollection();
+
+                  foreach (var path in inputPaths)
+                      imageCollection.Add(path);
+
+                  var spfImage = SpfImage.FromMagickImageCollection(imageCollection);
+                  spfImage.WriteSpf(o.Output);
+              }
+              else if (o.FromSpf)
+              {
+                  var spfImage = SpfImage.Read(o.Input);
+                  spfImage.WriteImg(o.Output);
+              }
+          });
+          
+          
+Console.ReadLine();
